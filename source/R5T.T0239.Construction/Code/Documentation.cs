e6609ba7c7883e0b01683173bb794c248ed4ec07
davidcoats <david@getsafetycone.com>
2024-01-17 13:36:29 -0800

    Initial commit.

---
+++ source/R5T.T0239.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0239.Construction
+{
+	/// <summary>
+	/// Construction console project for the R5T.T0239 library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0239/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0239
+{
+	/// <summary>
+	/// General property IHasX/IWithX strictly-framework types with no associated functionality types library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
