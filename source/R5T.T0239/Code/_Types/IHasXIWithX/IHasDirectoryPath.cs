a48ea3e2cf0c9432778fbf8ff6d58ed2fea6f0ce
davidcoats <david@getsafetycone.com>
2024-02-03 18:24:13 -0800

    Interim changes.

--- source/R5T.T0239/Code/_Types/IHasXIWithX/IHasDirectoryPath.cs
+++ source/R5T.T0239/Code/_Types/IHasXIWithX/IHasDirectoryPath.cs
@@ -7,6 +7,8 @@ using R5T.T0240;
 namespace R5T.T0239
 {
     /// <summary>
+    /// OBSOLETE - See R5T.L0093.T000.
+    /// 
     /// Has a stringly-typed directory path.
     /// </summary>
     [HasXMarker, DataTypeMarker]
--- source/R5T.T0239/Code/_Types/IHasXIWithX/IWithDirectoryPath.cs
+++ source/R5T.T0239/Code/_Types/IHasXIWithX/IWithDirectoryPath.cs
@@ -6,6 +6,9 @@ using R5T.T0240;
 
 namespace R5T.T0239
 {
+    /// <summary>
+    /// OBSOLETE - See R5T.L0093.T000.
+    /// </summary>
     [WithXMarker, DataTypeMarker]
     public interface IWithDirectoryPath :
         IHasDirectoryPath
